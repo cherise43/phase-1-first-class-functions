0149ad9b56174786647fc1da0195fa61529a1aa3
Cherise <nashpaiessy@gmail.com>
2023-06-21 20:47:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-function receivesAFunction(callback) {
-    callback();
+//callback functions
+
+//take a callback function as an argument
+//call the callback function
+//it doesn't matter what this function returns,
+// so long as it calls the callback function
+
+
+function receivesAFunction(callback){
+  callback();
+}
+
+//take no arguments
+//return a named function
+
+
+function returnsANamedFunction(){
+  return function fn(){
+      "Returns an anonymous function"
   }
-  
-  function returnsANamedFunction() {
-    return function namedFn() {
-      console.log("this is a named function");
-    };
+}
+//take no arguments
+//return an anonymous function
+
+
+function returnsAnAnonymousFunction(){
+  return function(){
+      "Returns an anonymous function"
   }
-  
-  function returnsAnAnonymousFunction() {
-    return () => console.log("this is an anonymous function");
-  }
+}
+
   
